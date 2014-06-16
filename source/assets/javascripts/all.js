e5233cf4262b37b34aa0f28ec4f7e419f7fd77a7
safiyesepetci <ssepetci11@gmail.com>
2014-06-16 16:31:52 +0300

    js codes removed

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,6 +1,2 @@
 //= require_tree .
 
-document.getElementById('forum_embed').src =
-'https://groups.google.com/forum/embed/?place=forum/bulutfon'
-+ '&showsearch=true&showpopout=true&showtabs=false'
-+ '&parenturl=' + encodeURIComponent(window.location.href);
